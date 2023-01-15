fcfc9a42762a3953ad5cd725ac71782eae7adb86
Ashley <ashuhleyt@gmail.com>
2023-01-15 15:20:34 -0800

    get services and tests setup

---
+++ spec/services/unsplash_service_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+RSpec.describe UnsplashService do 
+  it 'returns images for a given country' do 
+    country = 'Spain'
+    images = UnsplashService.retrieve_images(country)
+
+    expect(images).to be_a(Hash)
+    expect(images[:results]).to be_an(Array)
+
+    images[:results].each do |image|
+      expect(image).to have_key(:alt_description)
+      expect(image).to have_key(:urls)
+      expect(image[:urls]).to be_a(Hash)
+      expect(image[:urls]).to have_key(:regular)
+      expect(image[:urls][:regular]).to be_a(String)
+    end
+  end
+end
---
+++ spec/services/video_service_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+RSpec.describe VideoService do 
+  it 'returns helpful videos for given country' do 
+    country = 'Spain'
+    videos = VideoService.retrieve_videos(country)
+    expect(videos).to be_a(Hash)
+    expect(videos[:items]).to be_an(Array)
+    expect(videos[:items][0]).to have_key(:snippet)
+    expect(videos[:items][0][:snippet]).to be_a(Hash)
+    expect(videos[:items][0][:snippet]).to have_key(:title)
+    expect(videos[:items][0][:snippet][:title]).to be_a(String)
+    expect(videos[:items][0]).to have_key(:id)
+    expect(videos[:items][0][:id]).to be_a(Hash)
+    expect(videos[:items][0][:id]).to have_key(:videoId)
+    expect(videos[:items][0][:id][:videoId]).to be_a(String)
+
+    expect(videos[:items][0]).to_not have_key(:pixel)
+  end
+end
